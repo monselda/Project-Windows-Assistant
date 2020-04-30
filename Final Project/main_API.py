f11d4896298f46265267f895d67c9650167aef50
Hannes0730 <61642976+Hannes0730@users.noreply.github.com>
2020-04-30 22:53:23 +0800

    Delete main_API.py

--- Final Project/main_API.py
+++
@@ -1,9 +0,0 @@
-weather_api = ' '    #openweather API
-app_id = ' '                        #wolframAlpha ID
-
-
-#MySQL Credentials
-host=' '                                
-user=' '
-passwd=' ' 
-database=' '
